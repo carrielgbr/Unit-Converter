93fd7b342797ca708414278f13f92f5155036513
carriel <carrielgbr@gmail.com>
2025-11-08 23:54:43 -0300

    second commit for creating unit converter

--- int.cpp
+++ int.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main () {
 
-    int myNum = 15;
-    cout << myNum;
+    int myNum = 15; //declaring varible 15
+    myNum = 10; // now variable is 10 
+    cout << myNum; //Output 10
     return 0;
 
 }
